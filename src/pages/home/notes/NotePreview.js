43609767bcbaf990d6c3c7a06406b4d47fe71fc1
Sergey Podgayskiy <sergeypodgayskiy@gmail.com>
2020-02-17 15:26:53 +0300

    'setActiveNote' functionality update

--- src/pages/home/notes/NotePreview.js
+++ src/pages/home/notes/NotePreview.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Note = ({ value: note, activeNote, handleSetActiveNote }) => {
   return (
-    <li className={`note-preview ${note.id === activeNote.id ? 'active' : ''}`} onClick={e => handleSetActiveNote(note.id)}>
+    <li className={`note-preview ${note.id === activeNote && activeNote.id ? 'active' : ''}`} onClick={e => handleSetActiveNote(note.id)}>
       <span className="note-preview__title">{note.title}</span>
     </li>
   );
